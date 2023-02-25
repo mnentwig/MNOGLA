57f65023c70f44385d3619261110310e76c5b6d4
Markus Nentwig <mn83444@gmail.com>
2023-02-26 00:06:38 +0100

    cleanup

--- gui/guiContainer.h
+++ gui/guiContainer.h
@@ -12,7 +12,7 @@
 #include "../uiEvtListener/ptrEvtListener.h"
 
 namespace MNOGLA {
-using ::glm::vec2, ::glm::vec3;
+using ::glm::vec2, ::glm::vec3, ::glm::mat3;
 using ::std::shared_ptr, ::std::make_shared, ::std::string, ::std::vector;
 class guiButton {
    public:
@@ -22,17 +22,17 @@ class guiButton {
         if (clickCb != nullptr)
             clickCb();
     }
-    glm::vec2 getTopLeft() const {
-        return glm::vec2(x, y);
+    vec2 getTopLeft() const {
+        return vec2(x, y);
     }
-    glm::vec2 getBottomRight() const {
-        return glm::vec2(x + w, y + h);
+    vec2 getBottomRight() const {
+        return vec2(x + w, y + h);
     }
     void render(MNOGLA::twoDView& v) {
-        const glm::vec3& drawBgCol = preClickState ? bgColPreClick : bgCol;
-        const glm::vec3& drawTextCol = preClickState ? textColPreClick : textCol;
+        const vec3& drawBgCol = preClickState ? bgColPreClick : bgCol;
+        const vec3& drawTextCol = preClickState ? textColPreClick : textCol;
         v.filledRect(getTopLeft(), getBottomRight(), drawBgCol);
-        v.vectorText(getTopLeft(), text, h, drawTextCol);
+        v.vectorText(getTopLeft() + vec2(h / 4, 0), text, h, drawTextCol);
     }
 
     vec3 bgCol = vec3(0.2f, 0.2f, 0.2f);
@@ -74,16 +74,16 @@ class guiContainer : public ptrEvtListener {
 
     void autoscale() {
         struct {
-            void enterPt(const glm::vec2& pt) {
-                minPt = glm::vec2(::std::min(minPt.x, pt.x), ::std::min(minPt.y, pt.y));
-                maxPt = glm::vec2(::std::max(maxPt.x, pt.x), ::std::max(maxPt.y, pt.y));
+            void enterPt(const vec2& pt) {
+                minPt = vec2(::std::min(minPt.x, pt.x), ::std::min(minPt.y, pt.y));
+                maxPt = vec2(::std::max(maxPt.x, pt.x), ::std::max(maxPt.y, pt.y));
             }
-            glm::vec2 getCenter() { return (minPt + maxPt) / 2.0f; }
-            glm::vec2 getWh() { return (maxPt - minPt); }
+            vec2 getCenter() { return (minPt + maxPt) / 2.0f; }
+            vec2 getWh() { return (maxPt - minPt); }
 
            protected:
-            glm::vec2 minPt = glm::vec2(::std::numeric_limits<float>::infinity(), ::std::numeric_limits<float>::infinity());
-            glm::vec2 maxPt = glm::vec2(-::std::numeric_limits<float>::infinity(), -::std::numeric_limits<float>::infinity());
+            vec2 minPt = vec2(::std::numeric_limits<float>::infinity(), ::std::numeric_limits<float>::infinity());
+            vec2 maxPt = vec2(-::std::numeric_limits<float>::infinity(), -::std::numeric_limits<float>::infinity());
 
         } autoscaler;
         for (auto b : buttons) {
@@ -96,9 +96,9 @@ class guiContainer : public ptrEvtListener {
         view.set(autoscaler.getCenter(), viewWh, 0.0f);
     }
 
-    bool evtPtr_preClick(const ::glm::vec2& ptNorm) {
+    bool evtPtr_preClick(const vec2& ptNorm) {
         MNOGLA::logI("pre-click %f %f", ptNorm.x, ptNorm.y);
-        glm::vec2 ptWorld = view.getScreen2world() * glm::vec3(ptNorm, 1.0f);
+        vec2 ptWorld = view.getScreen2world() * vec3(ptNorm, 1.0f);
         bool anyHit = false;
         for (auto b : buttons) {
             bool hit = b->ptInside(ptWorld);
@@ -110,13 +110,13 @@ class guiContainer : public ptrEvtListener {
         return anyHit;
     }
 
-    void evtPtr_secondary(const ::glm::vec2& ptNorm) {
+    void evtPtr_secondary(const vec2& ptNorm) {
         MNOGLA::logI("secondary click %f %f", ptNorm.x, ptNorm.y);
         for (auto b : buttons)
             b->setPreClickState(false);
     };
 
-    void evtPtr_confirmClick(const ::glm::vec2& ptNorm) {
+    void evtPtr_confirmClick(const vec2& ptNorm) {
         MNOGLA::logI("confirm click %f %f", ptNorm.x, ptNorm.y);
         for (auto b : buttons)
             if (b->getPreClickState()) {
@@ -132,43 +132,41 @@ class guiContainer : public ptrEvtListener {
         panDown = false;
     };
 
-    void evtPtr_drag(const glm::vec2& deltaNorm) {
-        glm::vec2 mouseDelta = view.getScreen2world() * glm::vec3(deltaNorm, 1.0f) - view.getScreen2world() * glm::vec3(0.0f, 0.0f, 1.0f);  // fixme
+    void evtPtr_drag(const vec2& deltaNorm) {
+        vec2 mouseDelta = view.getScreen2world() * vec3(deltaNorm, 1.0f) - view.getScreen2world() * vec3(0.0f, 0.0f, 1.0f);  // fixme
         // MNOGLA::logI("evtPtr drag %f %f", mouseDelta.x, mouseDelta.y);
-        glm::mat3 m = view.getWorld2screen();
+        mat3 m = view.getWorld2screen();
         m = m * twoDMatrix::translate(mouseDelta);
         view.setWorld2screen(m);
     }
 
     void evtMouseRaw_scroll(int32_t deltaX, int32_t deltaY) {
-        glm::vec2 mousePos = view.getScreen2world() * glm::vec3(getLastMouseNormalized(), 1.0f);
-        glm::mat3 m = view.getWorld2screen();
+        vec2 mousePos = view.getScreen2world() * vec3(getLastMouseNormalized(), 1.0f);
+        mat3 m = view.getWorld2screen();
         m = m * twoDMatrix::translate(mousePos);
         m = m * twoDMatrix::rot((deltaX + deltaY) * 15.0f * 3.1415926 / 180.0f);
         m = m * twoDMatrix::translate(-mousePos);
         view.setWorld2screen(m);
     }
 
-    void evtPtr_twoPtrDrag(const ::glm::vec2& pt1start_NDC, const ::glm::vec2& pt1stop_NDC,
-                           const ::glm::vec2& pt2start_NDC, const ::glm::vec2& pt2stop_NDC) {
-        // MNOGLA::logI("twoPtr drag\t%f\t%f", pt1start.x, pt1start.y);
-        // MNOGLA::logI("           \t%f\t%f", pt2start.x, pt2start.y);
-        ::glm::vec2 p1startW = view.NDC2world(pt1start_NDC);
-        ::glm::vec2 p1stopW = view.NDC2world(pt1stop_NDC);
-        ::glm::vec2 p2startW = view.NDC2world(pt2start_NDC);
-        ::glm::vec2 p2stopW = view.NDC2world(pt2stop_NDC);
+    void evtPtr_twoPtrDrag(const vec2& pt1start_NDC, const vec2& pt1stop_NDC,
+                           const vec2& pt2start_NDC, const vec2& pt2stop_NDC) {
+        vec2 p1startW = view.NDC2world(pt1start_NDC);
+        vec2 p1stopW = view.NDC2world(pt1stop_NDC);
+        vec2 p2startW = view.NDC2world(pt2start_NDC);
+        vec2 p2stopW = view.NDC2world(pt2stop_NDC);
 
         // pinch vectors before and after event
-        ::glm::vec2 vStop = p2stopW - p1stopW;
-        ::glm::vec2 vStart = p2startW - p1startW;
+        vec2 vStop = p2stopW - p1stopW;
+        vec2 vStart = p2startW - p1startW;
+
         float vStopLen = glm::length(vStop);
         float vStartLen = glm::length(vStart);
         float phi_rad = glm::orientedAngle(vStart / vStartLen, vStop / vStopLen);
+        vec2 centerStart = (p1startW + p2startW) / 2.0f;
+        vec2 centerStop = (p1stopW + p2stopW) / 2.0f;
 
-        ::glm::vec2 centerStart = (p1startW + p2startW) / 2.0f;
-        ::glm::vec2 centerStop = (p1stopW + p2stopW) / 2.0f;
-
-        glm::mat3 m = view.getWorld2screen();
+        mat3 m = view.getWorld2screen();
         m = m *
             twoDMatrix::translate(centerStop) *        // step 4: origin to center of stop line
             twoDMatrix::rot(-phi_rad) *                // step 3: rotate to new direction
@@ -182,7 +180,7 @@ class guiContainer : public ptrEvtListener {
     MNOGLA::twoDView view;
 
     // === pan controls ===
-    glm::vec2 panDownPt;
+    vec2 panDownPt;
     bool panDown;
 };
 }  // namespace MNOGLA
