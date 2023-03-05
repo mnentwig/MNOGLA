eef82d746fa092c18d355f39699cb4f0b3b2ae85
Markus Nentwig <mn83444@gmail.com>
2023-03-05 16:21:43 +0100

    fixed static build for easy debug without DLLs in same folder

--- core/MNOGLA_includeGl.h
+++ core/MNOGLA_includeGl.h
@@ -1,13 +1,21 @@
 #pragma once
+extern "C" {
 #ifdef MNOGLA_WINDOWS
 // Windows version uses glew as it comes with its own copy of relevant headers
 // see also host side main.cpp (requires runtime initialization)
+
+// disable DLL decorations - they aren't needed for linking against the dll but break static linkage
+#define GLEW_STATIC 
+
 #include <GL/glew.h>
+
+// we get headers from glew - don't try to load via glfw
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #else
 #include <GLES3/gl31.h>
 #endif
-#define GLFW_INCLUDE_NONE
+}
 
 #include <cassert>
 #include <stdexcept>
