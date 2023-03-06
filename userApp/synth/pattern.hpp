fa6c0f5986816a92016e0522bed59f17c63fd77a
Markus Nentwig <mn83444@gmail.com>
2023-03-06 22:39:24 +0100

    pattern

--- userApp/synth/pattern.hpp
+++ userApp/synth/pattern.hpp
@@ -1,12 +1,22 @@
 #include <cstdio>
+#include <map>
 #include <stdexcept>
 #include <string>
+#include <vector>
 
 #include "../odsLoader/odsLoader.hpp"
-using std::string, std::runtime_error;
+using std::string, std::to_string, std::runtime_error, std::vector, std::map;
 class pattern {
    public:
-    pattern(const MNOGLA::odsDoc::block& data) : name(), num(0), denom(0) {
+    pattern(const MNOGLA::odsDoc::block& data) : name(), denom(4), notes() {
+        map<string, int> notename2midi;
+        const vector<string> notenames{"c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"};
+        for (int oct = 1; oct < 7; ++oct)
+            for (size_t note = 0; note < notenames.size(); ++note)
+                notename2midi.insert({notenames[note] + to_string(oct), note + 12 * oct + 12});
+
+        // === parse parameters ===
+        // starting 2nd row, first column
         for (size_t ixRow = data.getIxRow() + 1; ixRow < data.getIxRow() + data.getNRows(); ixRow += 2) {
             string key;
             if (!data.getDoc().getCell(data.getIxSheet(), ixRow, data.getIxCol(), key)) throw runtime_error("?? empty cell in first block col (key)??");
@@ -14,19 +24,34 @@ class pattern {
             if (!data.getDoc().getCell(data.getIxSheet(), ixRow + 1, data.getIxCol(), val)) throw runtime_error("?? empty cell in first block col (val)??");
             if (key == "name") {
                 name = val;
-            } else if (key == "num") {
-                if (sscanf(val.c_str(), "%llu", &this->num) != 1) throw runtime_error("pattern: failed to convert num");
             } else if (key == "denom") {
-                if (sscanf(val.c_str(), "%llu", &this->num) != 1) throw runtime_error("pattern: failed to convert denom");
+                if (sscanf(val.c_str(), "%llu", &this->denom) != 1) throw runtime_error("pattern: failed to convert denom");
             } else
                 throw runtime_error("pattern: invalid key '" + key + "'");
         }
-    }
 
+        // === parse sequence ===
+        // starting 2nd row, 2nd column
+        for (size_t ixCol = data.getIxCol() + 1; ixCol < data.getIxCol() + data.getNCols(); ++ixCol) {
+            string val;
+            if (!data.getDoc().getCell(data.getIxSheet(), data.getIxRow() + 1, ixCol, val))
+                val = "";
+            if (val == "")
+                notes.push_back(0);  // pause
+            else if (val == "-")
+                notes.push_back(-1);  // sustain
+            else {
+                auto it = notename2midi.find(val);
+                if (it == notename2midi.end()) throw runtime_error("failed to parse " + val);
+                notes.push_back(it->second);
+            }
+        }
+    }
     const string& getName() const { return name; }
+    const size_t getDenom() const { return denom; }
 
    protected:
     string name;
-    size_t num;
     size_t denom;
+    vector<int> notes;
 };
