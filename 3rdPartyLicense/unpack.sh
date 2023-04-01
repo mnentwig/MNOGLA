b0404fb1f2b53d8dfedbb165e672a8d4a80ce046
Markus Nentwig <mn83444@gmail.com>
2023-04-01 12:33:50 +0200

    freetype cont'd

---
+++ 3rdPartyLicense/unpack.sh
@@ -0,0 +1,24 @@
+rm -Rf Noto_Sans
+unzip -d Noto_Sans Noto_Sans.zip
+
+rm -Rf freetype-VER-2-13-0
+bzip2 -dc freetype-VER-2-13-0.tar.bz2 | tar -xvf -
+# see INSTALL.ANY for each module's required .c files
+sed -e '/t1_driver_class/ s/^/\/\//' -i freetype-VER-2-13-0/include/freetype/config/ftmodule.h
+sed -e '/cff_driver_class/ s/^/\/\//' -i freetype-VER-2-13-0/include/freetype/config/ftmodule.h
+sed -e '/t1cid_driver_class/ s/^/\/\//' -i freetype-VER-2-13-0/include/freetype/config/ftmodule.h
+sed -e '/pfr_driver_class/ s/^/\/\//' -i freetype-VER-2-13-0/include/freetype/config/ftmodule.h
+sed -e '/t42_driver_class/ s/^/\/\//' -i freetype-VER-2-13-0/include/freetype/config/ftmodule.h
+sed -e '/winfnt_driver_class/ s/^/\/\//' -i freetype-VER-2-13-0/include/freetype/config/ftmodule.h
+sed -e '/pcf_driver_class/ s/^/\/\//' -i freetype-VER-2-13-0/include/freetype/config/ftmodule.h
+sed -e '/bdf_driver_class/ s/^/\/\//' -i freetype-VER-2-13-0/include/freetype/config/ftmodule.h
+sed -e '/psaux_module_class/ s/^/\/\//' -i freetype-VER-2-13-0/include/freetype/config/ftmodule.h
+sed -e '/psnames_module_class/ s/^/\/\//' -i freetype-VER-2-13-0/include/freetype/config/ftmodule.h
+sed -e '/pshinter_module_class/ s/^/\/\//' -i freetype-VER-2-13-0/include/freetype/config/ftmodule.h
+sed -e '/ft_smooth_renderer_class/ s/^/\/\//' -i freetype-VER-2-13-0/include/freetype/config/ftmodule.h
+sed -e '/ft_sdf_renderer_class/ s/^/\/\//' -i freetype-VER-2-13-0/include/freetype/config/ftmodule.h
+sed -e '/ft_bitmap_sdf_renderer_class/ s/^/\/\//' -i freetype-VER-2-13-0/include/freetype/config/ftmodule.h
+sed -e '/ft_svg_renderer_class/ s/^/\/\//' -i freetype-VER-2-13-0/include/freetype/config/ftmodule.h
+
+rm -Rf glm-master
+unzip glm-0.9.9.9.zip
--- host_mingw64linux/mingwInstallPackages.sh
+++ host_mingw64linux/mingwInstallPackages.sh
@@ -2,3 +2,4 @@ pacman -S --needed base-devel mingw-w64-x86_64-toolchain
 pacman -S mingw-w64-x86_64-glm
 pacman -S mingw-w64-x86_64-glfw
 pacman -S mingw-w64-x86_64-glew
+pacman -S unzip
